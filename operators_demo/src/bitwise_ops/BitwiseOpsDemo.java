b514943b42e70cf5aa23c6499802d3d7faf9d0dc
R3S3ARCH3R-6 <R3S3ARCH3R-6@users.noreply.github.com>
2020-09-22 16:06:41 -0500

    finished project

--- operators_demo/src/bitwise_ops/BitwiseOpsDemo.java
+++ operators_demo/src/bitwise_ops/BitwiseOpsDemo.java
@@ -3,17 +3,14 @@ package bitwise_ops;
 public class BitwiseOpsDemo {
 
 	public static void main(String[] args) {
-		byte a = 0100;
-		byte b = 100;
-		byte c = 0110;
 		
-		System.out.println(a);
-		
-		System.out.println("Bitwise AND operator: = "  );
-		System.out.println("Bitwise OR operator:  = " );
-		System.out.println("Bitwise NOT operator:  = " );
-		System.out.println("Bitwise XOR operator:  = " );
-		
+		System.out.println("Bitwise AND operator: (13 & 4) = (1101 & 0100) = 0100 = " + (13 & 4));
+		System.out.println("Bitwise OR operator: (7 | 2) = (0111 | 0010) = 0111" +(7 | 2));
+		System.out.println("Bitwise NOT operator: (~7) = ~(00111) = 11000 = " + (~7));
+		System.out.println("Bitwise XOR operator: (11 ^ 4) = (1011 ^ 0100) = 1111" + (11 ^ 4) );
+		System.out.println("Bitwise left shift: (8 << 1) = (1000 << 1) = 10000 = " + (8 << 1) );
+		System.out.println("Bitwise right shift: (10 >> 1) = (1010 >> 1) = 0101 = " + (10 >> 1));
+		System.out.println("Bitwise zero-fill right shift: (15 >>> 1) = (1111 >>> 1) = 0111 = " + (15 >>> 1) );
 	}
 
 }
