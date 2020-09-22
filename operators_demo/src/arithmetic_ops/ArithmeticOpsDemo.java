59426aa31bb4fd92fe735ffc179019d6399bd82c
R3S3ARCH3R-6 <R3S3ARCH3R-6@users.noreply.github.com>
2020-09-22 16:09:02 -0500

    forgot to remove unncessary methods

--- operators_demo/src/arithmetic_ops/ArithmeticOpsDemo.java
+++ operators_demo/src/arithmetic_ops/ArithmeticOpsDemo.java
@@ -1,26 +1,6 @@
 package arithmetic_ops;
 
 public class ArithmeticOpsDemo {
-
-	public double add(double a, double b){
-		return a + b;
-	}
-	
-	public double subtract(double a, double b){
-		return a - b;
-	}
-	
-	public double multiply(double a, double b){
-		return a * b;
-	}
-	
-	public double divide(double a, double b){
-		return a/b;
-	}
-	
-	public double mod(double a, double b){
-		return a % b;
-	}
 	
 	public static void main(String[] args) {
 		float a = 9f;
